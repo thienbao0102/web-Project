9777dbe2e3647c30acc34554c01c3b08d4b0a970
Hoai-pc <machhoai1234@gmail.com>
2024-04-12 12:50:47 +0700

    index html & css

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ const controllerDetail = require('./controllers/controllers')
 const app = express()
 const port = 3000
 
+app.use(express.static('public/images'));
+app.use(express.static('public/pages/stylesheets'));
+//app.use(express.static('public/pages/scipts'));
+//app.use(express.static('public/images/svg'));
+
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => res.send('Hello World!'))
+/*------------Route------------*/
+app.get('/', async (req, res) =>{
+    res.sendFile(__dirname + '/public/pages/index.html');
+})
+
+
+
+/*------------API------------*/
+
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
