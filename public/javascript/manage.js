70c56ebd491434e1936b394096f09c71a48a5271
Bao <nguyenbao01022004@gmail.com>
2024-04-17 16:02:19 +0700

    finish management page

--- public/javascript/manage.js
+++ public/javascript/manage.js
@@ -11,5 +11,19 @@ function extendSearch(){
 //hien thi o cap nhat
 const FormUpdate = document.querySelector('.update');
 function showFormUpdate(){
-    FormUpdate.classList.toggle('active')
+    FormUpdate.classList.toggle('active');
 }
+
+//cho phép nhap quantity khi da nhap size
+const size = document.getElementById('sizeUpdate');
+const quantity = document.getElementById('quantityUpdate')
+
+size.addEventListener('change', ()=>{
+    console.log(size.value);
+    if(size.value > 0 && size.value <= 60){
+        quantity.disabled = false;
+    }
+    else{
+        quantity.disabled = true;
+    }
+})
